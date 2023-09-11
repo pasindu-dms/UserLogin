bb2f81ebbfaf91da6f936b3785497b011d9d05d6
pasindu-dms <pasindudms@gmail.com>
2023-09-11 22:57:37 +0530

    push

---
+++ UserSignupLogin/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserSignupLogin.Models;
+
+namespace UserSignupLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBuserSignupLoginEntities db = new DBuserSignupLoginEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.TBLUserInfoes.ToList());
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Signup(TBLUserInfo tBLUserInfo)
+
+        {
+            if(db.TBLUserInfoes.Any(x=>x.UsernameUs == tBLUserInfo.UsernameUs))
+            {
+                ViewBag.notification = "This account has already existed";
+                return View();
+            }
+            else
+            {
+                db.TBLUserInfoes.Add(tBLUserInfo);
+                db.SaveChanges();
+
+                Session["idusSS"] = tBLUserInfo.IdUs.ToString();
+                Session["UsernameSS"] = tBLUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Login(TBLUserInfo tBLUserInfo)
+        {
+            var checkLogin = db.TBLUserInfoes.Where(x => x.UsernameUs.Equals(tBLUserInfo.UsernameUs) && x.PasswordUs.Equals(tBLUserInfo.PasswordUs)).FirstOrDefault();
+            if (checkLogin != null)
+            {
+                Session["idusSS"] = tBLUserInfo.IdUs.ToString();
+                Session["UsernameSS"] = tBLUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "name");
+            }
+            else
+            {
+                ViewBag.Notification = "Worng Username or Password";
+            }
+            return View();
+        }
+        
+    }
+}
